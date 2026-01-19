7f4af2c3a4e22b89460b3223b6656d24c095ccaf
Dyluxd <arisitadiehz@gmail.com>
2026-01-19 12:07:41 -0600

    Entreguita Area Volumen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int main() {
+        float L;
+
+            int area;
+            int volumen;
+    double side, area, volume;
+
+    printf("introduce long de la longitud del lado:");
+    scanf("%lf", &side);
+
+    area = L * L;
+    volume = L * L * L;
+
+    printf("area:%1\nvolumen:%1\n", area, volumen);
+
     return 0;
 }
